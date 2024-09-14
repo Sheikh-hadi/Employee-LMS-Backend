8a5d7bb6b7d32793f2eeebd12970864705eb0ffd
Sheikh-hadi <96363461+Sheikh-hadi@users.noreply.github.com>
2024-09-14 12:32:12 +0500

    ++

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import { ApiResponse } from "../utilis/ApiResponse.js";
 
 
 const auth = AsyncHandler(async (req, res, next) => {
+    console.log("req.user", req.user)
     return res.status(200).json(new ApiResponse(200, req.user, "User is authenticated"))
 })
 
